c0d5accc941133c69fd430152ab66231c608d2b0
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2026-04-07 19:18:57 -0400

    service class

--- W26W13AspDotNetWithDatabase/Services/ProductService.cs
+++ W26W13AspDotNetWithDatabase/Services/ProductService.cs
@@ -17,5 +17,33 @@ namespace W26W13AspDotNetWithDatabase.Services
         {
             return await _context.Products.ToListAsync();
         }
+
+        public async Task<Product?> GetProductByIdAsync(int id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+
+        public async Task AddProductAsync(Product product)
+        {
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateProductAsync(Product product)
+        {
+            _context.Products.Update(product);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteProductAsync(int id)
+        {
+            var prod = await _context.Products.FindAsync(id);
+
+            if (prod != null)
+            {
+                _context.Products.Remove(prod);
+                await _context.SaveChangesAsync();
+            }
+        }
     }
 }
